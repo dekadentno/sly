46fbb5e13d790a86c1470bf59d5b7691ad7315e7
dekadentno <matej.lazarevich@gmail.com>
2024-01-22 23:27:36 +0100

    Implement sly.py, adapt readme

--- README.md
+++ README.md
@@ -1,7 +1,54 @@
 # sly
-Curation of open-source tools and scripts, tailored for red-teaming activities and OSCP preparation. Use these tools strictly for research and ethical learning.
+Curation of open-source tools and scripts, now streamlined through Sly, a script tailored for red-teaming activities and OSCP preparation.
 
-Sly is your lazy-but-smart hub for red-teaming and OSCP tools, saving you from the tedious chore of hunting down each script every single time.
+Sly, your lazy-but-smart script, automatically collects red-teaming and OSCP tools, saving you the tedious chore of hunting down each script every single time.
+
+Use these tools strictly for research and ethical learning.
+
+## Usage
+```bash
+# Clone the Sly repository
+git clone https://github.com/dekadentno/sly.git
+
+# Run the Sly script
+cd sly
+python3 sly.py
+
+# or, if the script needs executable permission
+chmod +x sly.py
+./sly.py
+
+# With default settings sly will put everything into the default "sly" directory
+python3 sly.py 
+
+# To specify a different destination directory
+python3 sly.py -d /path/to/destination
+
+# Or just run it with this neat one liner
+curl -sSL https://raw.githubusercontent.com/dekadentno/sly/main/sly.py | python3 -
+```
+
+This script was designed and tested for Kali Linux and may not work on other systems because it uses paths specific to Kali.
+```bash
+mlay in ~/projects/sly on main ● ● λ cat /etc/os-release
+
+PRETTY_NAME="Kali GNU/Linux Rolling"
+NAME="Kali GNU/Linux"
+VERSION_ID="2023.3"
+VERSION="2023.3"
+VERSION_CODENAME=kali-rolling
+ID=kali
+ID_LIKE=debian
+HOME_URL="https://www.kali.org/"
+SUPPORT_URL="https://forums.kali.org/"
+BUG_REPORT_URL="https://bugs.kali.org/"
+ANSI_COLOR="1;31"
+
+
+mlay in ~/projects/sly on main ● ● λ uname -a
+
+Linux kali 6.3.0-kali1-arm64 #1 SMP Debian 6.3.7-1kali1 (2023-06-29) aarch64 GNU/Linux
+```
 
 ## Tools
 | Tool Name | Platform | Purpose | Has multiple Architectures |
@@ -9,39 +56,31 @@ Sly is your lazy-but-smart hub for red-teaming and OSCP tools, saving you from t
 | Mimikatz | Windows | Credential dumping and manipulation | false |
 | Unix-privesc-check | Linux | Checks for privilege escalation vectors | false |
 | Seatbelt | Windows | Security checks and system enumeration | false |
-| Netcat | Cross-platform | Networking utility for connections | false |
 | Ncat | Cross-platform | Enhanced version of Netcat | false |
 | LinPEAS | Linux | Privilege escalation checks | false |
 | WinPEAS | Windows | Windows privilege escalation checks | false |
 | ADpeas | Windows | Active Directory enumeration | false |
 | PrintSpoofer64 | Windows | Abusing print spooler services | false |
 | GodPotato | Windows | Exploiting Windows BITS service | false |
-| PowerShell Empire | Windows | Post-exploitation framework | false |
 | BloodHound | Windows | AD Trust analysis | false |
 | PowerSploit | Windows | PowerShell-based exploitation | false |
-| Enum4linux | Linux | Enumerating Windows and Samba systems | false |
 | WPScan | Cross-platform | WordPress vulnerability scanner | false |
 | linpeas | Cross-platform | Privilege escalation tools | true |
 | winpeas | Cross-platform | Privilege escalation tools | true |
 | Rubeus | Windows | Kerberos attack framework | false |
-| Veil | Cross-platform | Evasion toolkits | false |
 | PsTools | Windows | Suite of command-line Windows utilities | false |
 | Kerbrute | Cross-platform | Brute-forcing Kerberos pre-auth | false |
 | nc64.exe | Windows | 64-bit Netcat for Windows | false |
 | PowerView | Windows | AD enumeration | false |
 | SharpHound | Windows | Collecting AD data | false |
 | Spray-Passwords.ps1 | Windows | Password spraying script | false |
-| Runas | Windows | Running commands as different user | false |
-| PsExec | Windows | Remote command execution | false |
-| Process Explorer | Windows | Advanced task manager | false |
-| Process Monitor (Procmon) | Windows | System monitoring tool | false |
-| PsLoggedOn | Windows | Checking logged on users | false |
-| vshadow | Windows | Volume Shadow Copy management | false |
+| Sysinternals Suite | Windows | Volume Shadow Copy management | true |
 | powercat.ps1 | Windows | PowerShell-based Netcat alternative | false |
-| bypass.ps1 | Windows | Script to bypass AMSI and script defender | false |
 | powerup.ps1 | Windows | PowerShell script for privilege escalation | false |
-| nttokenfunctions.ps1 | Windows | Token manipulation and enumeration | false |
-| enumeration.ps1 | Windows | Script for AD enumeration | false |
-| powerview.ps1 | Windows | Advanced AD enumeration | false |
 | spray-passwords.ps1 | Windows | Password spraying tool | false |
 | DomainPasswordSpray.ps1 | Windows | Domain-wide password spraying tool | false |
+
+## Todo
+
+## Disclaimer
+See [Disclaimer](./DISCLAIMER.md).
---
+++ sly.py
@@ -0,0 +1,156 @@
+import os
+import urllib.request
+import zipfile
+import argparse
+
+failed_downloads = []
+tools_without_urls = []
+tools = [
+    {'name': 'Mimikatz', 'paths': ['/usr/share/windows-resources/mimikatz/x64/mimikatz.exe', '/usr/share/windows-resources/mimikatz/Win32/mimikatz.exe']},
+    {'name': 'Unix-privesc-check', 'paths': ['/usr/share/unix-privesc-check/unix-privesc-check']},
+    {'name': 'Seatbelt', 'paths': ['/usr/share/powershell-empire/empire/server/modules/powershell/situational_awareness/host/seatbelt.py']},
+    {'name': 'Ncat', 'paths': ['/usr/bin/ncat']},
+    {'name': 'LinPEAS', 'paths': ['/usr/share/peass/linpeas/linpeas_linux_386', '/usr/share/peass/linpeas/linpeas_linux_amd64', '/usr/share/peass/linpeas/linpeas_linux_arm', '/usr/share/peass/linpeas/linpeas_linux_arm64']},
+    {'name': 'WinPEAS', 'paths': ['/usr/share/peass/winpeas/winPEASany.exe', '/usr/share/peass/winpeas/winPEASx64.exe']},
+    {'name': 'ADpeas', 'paths': ['https://raw.githubusercontent.com/61106960/adPEAS/main/adPEAS.ps1']},
+    {'name': 'PrintSpoofer64', 'paths': ['https://github.com/itm4n/PrintSpoofer/releases/download/v1.0/PrintSpoofer64.exe', 'https://github.com/itm4n/PrintSpoofer/releases/download/v1.0/PrintSpoofer32.exe']},
+    {'name': 'GodPotato', 'paths': ['https://github.com/BeichenDream/GodPotato/releases/download/V1.20/GodPotato-NET4.exe', 'https://github.com/BeichenDream/GodPotato/releases/download/V1.20/GodPotato-NET35.exe', 'https://github.com/BeichenDream/GodPotato/releases/download/V1.20/GodPotato-NET2.exe']},
+    {'name': 'BloodHound', 'paths': []},
+    {'name': 'PowerSploit', 'paths': ['https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Recon/PowerView.ps1']},
+    {'name': 'WPScan', 'paths': ['/usr/bin/wpscan']},
+    {'name': 'linpeas', 'paths': ['/usr/share/peass/linpeas/linpeas_linux_amd64']},
+    {'name': 'winpeas', 'paths': ['/usr/share/peass/winpeas/winPEASx64.exe']},
+    {'name': 'Rubeus', 'paths': ['https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/Rubeus.exe']},
+    {'name': 'PsTools', 'paths': ['https://download.sysinternals.com/files/PSTools.zip']},
+    {'name': 'Kerbrute', 'paths': ['https://github.com/ropnop/kerbrute/releases/download/v1.0.3/kerbrute_windows_amd64.exe', 'https://github.com/ropnop/kerbrute/releases/download/v1.0.3/kerbrute_windows_386.exe']},
+    {'name': 'nc64.exe', 'paths': ['https://github.com/int0x33/nc.exe/raw/master/nc64.exe']},
+    {'name': 'PowerView', 'paths': ['https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Recon/PowerView.ps1']},
+    {'name': 'SharpHound', 'paths': []},
+    {'name': 'Sysinternals Suite', 'paths': ['https://download.sysinternals.com/files/SysinternalsSuite.zip']},
+    {'name': 'powercat.ps1', 'paths': ['/usr/share/powershell-empire/empire/server/data/module_source/management/powercat.ps1']},
+    {'name': 'powerup.ps1', 'paths': ['https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Privesc/PowerUp.ps1']},
+    {'name': 'spray-passwords.ps1', 'paths': ['./other/Spray-Passwords.ps1']},
+    {'name': 'DomainPasswordSpray.ps1', 'paths': ['https://raw.githubusercontent.com/dafthack/DomainPasswordSpray/master/DomainPasswordSpray.ps1']}
+]
+
+def download_and_extract_zip(url, destination):
+    try:
+        zip_file_name = os.path.basename(url)
+        zip_path = os.path.join(destination, zip_file_name)
+        extraction_path = os.path.join(destination, os.path.splitext(zip_file_name)[0])
+
+        with urllib.request.urlopen(url) as response:
+            with open(zip_path, 'wb') as out_file:
+                file_size = int(response.headers['content-length'])
+                print(f"Downloading {zip_file_name}")
+
+                downloaded = 0
+                chunk_size = 1024
+                while True:
+                    chunk = response.read(chunk_size)
+                    if not chunk:
+                        break
+                    out_file.write(chunk)
+                    downloaded += len(chunk)
+                    percent = downloaded / file_size * 100
+                    print(f"Downloaded {percent:.2f}%", end="\r", flush=True)
+        print("\n")
+
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            os.makedirs(extraction_path, exist_ok=True)
+            zip_ref.extractall(extraction_path)
+        os.remove(zip_path)
+        return True
+    except Exception as e:
+        print(f"\nError downloading or extracting {url}: {e}")
+        return False
+
+def download_file(url, destination):
+    try:
+        if url.endswith('.zip'):
+            return download_and_extract_zip(url, destination)
+        else:
+            file_path = os.path.join(destination, os.path.basename(url))
+            with urllib.request.urlopen(url) as response:
+                with open(file_path, 'wb') as out_file:
+                    file_size = int(response.headers['content-length'])
+                    print(f"Downloading {os.path.basename(file_path)}")
+
+                    downloaded = 0
+                    chunk_size = 1024
+                    while True:
+                        chunk = response.read(chunk_size)
+                        if not chunk:
+                            break
+                        out_file.write(chunk)
+                        downloaded += len(chunk)
+                        percent = downloaded / file_size * 100
+                        print(f"Downloaded {percent:.2f}%", end="\r", flush=True)
+            print("\n")
+            return True
+    except Exception as e:
+        print(f"\nError downloading {url}: {e}")
+        return False
+
+def copy_file(source, destination):
+    try:
+        if os.path.exists(source):
+            total_size = os.path.getsize(source)
+            copied = 0
+            chunk_size = 1024 * 1024  # 1MB chunk size
+
+            with open(source, 'rb') as src_file:
+                with open(os.path.join(destination, os.path.basename(source)), 'wb') as dst_file:
+                    while True:
+                        chunk = src_file.read(chunk_size)
+                        if not chunk:
+                            break
+                        dst_file.write(chunk)
+                        copied += len(chunk)
+                        percent = (copied / total_size) * 100
+                        print(f"Copying {os.path.basename(source)} - {percent:.2f}%", end="\r", flush=True)
+            print("\n")
+            return True
+        else:
+            print(f"File not found: {source}")
+            return False
+    except Exception as e:
+        print(f"\nError copying {source}: {e}")
+        return False
+
+def main(destination):
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+    
+    for tool in tools:
+        if not tool["paths"]:
+            tools_without_urls.append(tool["name"])
+            continue
+
+        for path in tool["paths"]:
+            if path.startswith("http://") or path.startswith("https://"):
+                if not download_file(path, destination):
+                    failed_downloads.append(path)
+            else:
+                if not copy_file(path, destination):
+                    failed_downloads.append(path)
+
+    if failed_downloads:
+        print("\nFailed to download the following tools:")
+        for path in failed_downloads:
+            print(f"- {path}")
+
+    if tools_without_urls:
+        print("\nThe following tools didn't have any URLs defined:")
+        for tool in tools_without_urls:
+            print(f"- {tool}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='This script downloads and/or copies a predefined list of tools to a specified directory. It handles both direct file copying and downloading from URLs, including handling zip files.')
+
+    parser.add_argument('-d', '--destination', help='The full path to the destination directory where tools will be downloaded/copied. If not specified, it defaults to a subdirectory in the current working directory.')
+    parser.add_argument('-f', '--folder', default="sly", help='Name of the subdirectory in the current working directory to use as the default destination. Defaults to "sly".')
+
+    args = parser.parse_args()
+    destination = args.destination if args.destination else os.path.join(os.getcwd(), args.folder)
+    main(destination)
